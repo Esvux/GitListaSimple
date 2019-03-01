103f89148fced35665f13281b95609b5f0eae92c
Esvin González <usac.ing.esvin@gmail.com>
2019-02-28 18:21:39 -0600

    Quitando comentarios inútiles.

--- Principal.java
+++ Principal.java
@@ -1,15 +1,6 @@
-import java.util.*;
-
 public class Principal {
 
 	public static void main(String[] args) {
-		/*
-		List<Persona> lista = new ArrayList<>(4);		
-		lista.add(new Persona("Juan", "Santizo"));
-		lista.add(new Persona("Esvin", "González"));
-		lista.add(null);
-		*/
-
 		Lista<Persona> listaArtesanal = new Lista<>();
 		listaArtesanal.agregar(new Persona("Juan", "Santizo"));
 		listaArtesanal.agregar(new Persona("Esvin", "González"));
