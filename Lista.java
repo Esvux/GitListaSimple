ddf330ef4506f49159181f79a0ee4a5667386035
Heros Noriega <hnoriegajuarez@gmail.com>
2019-03-05 14:36:12 -0600

    Funcion eliminar corregida

--- Lista.java
+++ Lista.java
@@ -2,51 +2,66 @@ public class Lista <T> {
 
 	//Variables globales - Atributo/Propiedad de clase
 	Nodo<T> inicio; //null
+	int longitud;
 
 	public Lista() {
 		this.inicio = null;
+		this.longitud = 0;
+		
 	}
 
 	public void agregar(T nuevo) {
 		if(this.inicio == null) {
 			//Es el primer elemento
 			this.inicio = new Nodo<>(nuevo);
+			longitud++;
 		} else {
 			//Ya hay al menos un elemento
 			Nodo<T> nuevoNodo = new Nodo<>(nuevo);
 			nuevoNodo.puntero = this.inicio;
 			this.inicio = nuevoNodo;
+			longitud++;
 		}
 	}
 
 	public void mostrar() {
 		Nodo<T> temp = this.inicio;
+		int contador = 1;
 		while(temp != null) {
-			System.out.println(temp.valor);
+			
+			System.out.println(contador + " : " + temp.valor);
+			contador++;
 			temp = temp.puntero;
 		}
 	}
 
 	public T eliminar(int i) {
-		if(i < 0) {
-			throw new IndexOutOfBoundsException("No se permite eliminar elementos con índice negativo.");
+		if(i < 0 ) {
+			System.out.println("No se permite eliminar elementos menor a 0. ");
 		}
-		Nodo<T> anterior = null;
+		
 		Nodo<T> actual = this.inicio;
-		int contador = 0;
-		while(actual != null) {
-			if(contador != i) {
-				//No es el elemento a eliminar
-				anterior = actual;
-				actual = actual.puntero;
-				contador++;
-				continue;
+		
+		//Si la posicion a eliminar es mayor o igual a 0 y menor o igual a la longitud de lista
+		if(i >= 0 && i <= longitud) {
+			
+			//Si la pocision es igual a 0
+			if(i == 0) {
+				this.inicio = actual.puntero;
+				longitud--;
+				
+			} else { //Si la posicion es mayor a 0
+				
+				for (int j = 0; j < i -1; j++) {
+					actual = actual.puntero;
+				}
+				actual.puntero = actual.puntero.puntero;
+				longitud--;
 			}
-			//Encontré el elemento a eliminar
-			anterior.puntero = actual.puntero;
-			return actual.valor;
 		}
-		throw new IndexOutOfBoundsException("Índice fuera de la lista " + i);
+		return actual.valor;
+		
+		
 	}
 
 }
