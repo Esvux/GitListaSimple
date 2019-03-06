eef00e0ee0dd9dff1c9032e375468437bc71a7a9
Heros Noriega <hnoriegajuarez@gmail.com>
2019-03-05 19:26:19 -0600

    Arreglada variable Contador

--- Lista.java
+++ Lista.java
@@ -3,11 +3,12 @@ public class Lista <T> {
 	//Variables globales - Atributo/Propiedad de clase
 	Nodo<T> inicio; //null
 	int longitud;
+	int contador;
 
 	public Lista() {
 		this.inicio = null;
 		this.longitud = 0;
-		
+
 	}
 
 	public void agregar(T nuevo) {
@@ -26,9 +27,8 @@ public class Lista <T> {
 
 	public void mostrar() {
 		Nodo<T> temp = this.inicio;
-		int contador = 1;
 		while(temp != null) {
-			
+
 			System.out.println(contador + " : " + temp.valor);
 			contador++;
 			temp = temp.puntero;
@@ -39,19 +39,19 @@ public class Lista <T> {
 		if(i < 0 ) {
 			System.out.println("No se permite eliminar elementos menor a 0. ");
 		}
-		
+
 		Nodo<T> actual = this.inicio;
-		
+
 		//Si la posicion a eliminar es mayor o igual a 0 y menor o igual a la longitud de lista
 		if(i >= 0 && i <= longitud) {
-			
+
 			//Si la pocision es igual a 0
 			if(i == 0) {
 				this.inicio = actual.puntero;
 				longitud--;
-				
+
 			} else { //Si la posicion es mayor a 0
-				
+
 				for (int j = 0; j < i -1; j++) {
 					actual = actual.puntero;
 				}
@@ -60,8 +60,8 @@ public class Lista <T> {
 			}
 		}
 		return actual.valor;
-		
-		
+
+
 	}
 
 }
