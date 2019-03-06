107b0f5d80bb99638f571dbeb0024004a55c2f2a
Heros Noriega <hnoriegajuarez@gmail.com>
2019-03-05 19:32:39 -0600

    i Variable corregida

--- Lista.java
+++ Lista.java
@@ -2,8 +2,10 @@ public class Lista <T> {
 
 	//Variables globales - Atributo/Propiedad de clase
 	Nodo<T> inicio; //null
+
 	int longitud;
 	int contador;
+	int i;
 
 	public Lista() {
 		this.inicio = null;
@@ -12,6 +14,7 @@ public class Lista <T> {
 	}
 
 	public void agregar(T nuevo) {
+
 		if(this.inicio == null) {
 			//Es el primer elemento
 			this.inicio = new Nodo<>(nuevo);
@@ -32,6 +35,7 @@ public class Lista <T> {
 			System.out.println(contador + " : " + temp.valor);
 			contador++;
 			temp = temp.puntero;
+			contador++;
 		}
 	}
 
@@ -61,6 +65,56 @@ public class Lista <T> {
 		}
 		return actual.valor;
 
+
+	}
+
+
+
+	public void counter(){
+		System.out.println("Numero de Items en la lista:" +contador);
+		throw new IndexOutOfBoundsException("Indice fuera de la lista " + i);
+	}
+
+	public T reemplazar(int i, T nuevo) {
+
+		if(i<0) {
+		    throw new IllegalStateException("El indice no puede ser negativo: " + i);
+        }
+
+		// What if inicio == null? Return it
+		if(this.inicio == null) {
+			throw new IllegalStateException("La lista esta vacia");
+		}
+
+        int index = 0;
+        Nodo<T> prev = this.inicio;
+
+        // Create new nodo
+        Nodo<T> newNodo = new Nodo<>(nuevo);
+
+		// What if i = 0? Replace inicio
+		if(i == 0) {
+		    T replaced = inicio.valor;
+			newNodo.puntero = inicio.puntero;
+			this.inicio = newNodo;
+			return replaced;
+		}
+
+		// Look for the previous one
+		while(index != i-1 && prev.puntero != null) {
+			prev = prev.puntero;
+			index++;
+		}
+
+		// Reached the end of the list, without finding the index
+		if(index == i-1 && prev.puntero == null) {
+			throw new IndexOutOfBoundsException("Posicion no existe " + i);
+		} else {
+			Nodo<T> replaced = prev.puntero;
+			newNodo.puntero = replaced.puntero;
+			prev.puntero = newNodo;
+			return replaced.valor;
+		}
 
 	}
 
